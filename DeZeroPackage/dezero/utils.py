456137b8fa8eb3b9bebc017a80e2035b840222cc
xiejingcheng <jingchengxie0131@gmail.com>
2024-01-11 20:58:43 +0800

    更新计算图可视化

---
+++ DeZeroPackage/dezero/utils.py
@@ -0,0 +1,67 @@
+import os
+import subprocess
+
+def _dot_var(v, verbose=False):
+    dot_var = '{} [label="{}", color=orange, style=filled]\n'
+
+    name = '' if v.name is None else v.name
+    if verbose and v.data is not None:
+        if v.name is not None:
+            name += ': '
+        name += str(v.shape) + ' ' + str(v.dtype)
+    return dot_var.format(id(v), name)
+
+def _dot_func(f):
+    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
+    txt = dot_func.format(id(f), f.__class__.__name__)
+
+    dot_edge = '{} -> {}\n'
+    for x in f.inputs:
+        txt += dot_edge.format(id(x), id(f))
+    for y in f.outputs:
+        txt += dot_edge.format(id(f), id(y()))
+    return txt
+
+def getDotGraph(output, verbose=True):
+    txt = ''
+    funcs = []
+    seen_set = set()
+
+    def addFunc(f):
+        if f not in seen_set:
+            funcs.append(f)
+            # funcs.sort(key=lambda x: x.generation)
+            seen_set.add(f)
+
+    addFunc(output.creator)
+    txt += _dot_var(output, verbose)
+
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.inputs:
+            txt += _dot_var(x, verbose)
+
+            if x.creator is not None:
+                addFunc(x.creator)
+    return 'digraph g {\n' + txt + '}'
+
+def plotDotGraph(output, verbose=True, to_file='graph.png'):
+    dot_graph = getDotGraph(output, verbose)
+
+    tmp_dir = os.path.join(os.path.expanduser('~'), '.dezero')
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
+    with open(graph_path, 'w') as f:
+        f.write(dot_graph)
+
+    extension = os.path.splitext(to_file)[1][1:]
+    cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
+    subprocess.run(cmd, shell=True)
+
+    try:
+        from IPython import display
+        return display.Image(filename=to_file)
+    except:
+        pass
